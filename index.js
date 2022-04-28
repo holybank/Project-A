d764c68b3479d2728ffa066f187d30cb412fe215
Thana Mannaisajjathum <thana@tripleaerp.com>
2022-04-28 16:32:39 +0700

    Heroku setup

--- index.js
+++ index.js
@@ -42,6 +42,8 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+let port = process.env.PORT;
+if (port == null || port == "") {port = 3000}
+server.listen(port, () => {
+    console.log('listening on *:'+port);
 });
